e64f64eb00a105c11f7a9ed155d3a65e71178ca5
sist <sist@DESKTOP-M509LHQ>
2020-04-07 10:33:08 +0900

    20200407 MMMMMMM

--- eHRGit0101/src/com/sist/ehr/HelloWorld.java
+++ eHRGit0101/src/com/sist/ehr/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, Gitttttt.");
 		System.out.println("Hellooo, Git.");
+		System.out.println("hahaaaaaaaaaaaa");
 
 	}
 
